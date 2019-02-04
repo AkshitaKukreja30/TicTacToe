7c7d68d569fd1145afe270867baf1538b281b103
Akshita Kukreja <akshita.kukreja@cybergindia.local>
2019-02-04 17:55:26 +0530

    Tic tac toe using HTML AND JS

---
+++ j1.js
@@ -0,0 +1,49 @@
+var i=1;
+var winner="";
+var gameEnded=false;
+var j=1;
+function mark(id) {
+  if(j==0){
+  	document.getElementById(id).disabled = true;
+  	alert(winner+ " has already won!")
+  	return;
+  }
+  if(i%2==0){
+  	document.getElementById(id).value="X";
+  	winner="Player X";
+    }
+  else{
+  	document.getElementById(id).value="0";
+   	winner="Player 0";
+  	  }
+	console.log("inside"+ id)
+	console.log("j = "+j)
+	if(j==1){
+		if(gameEnded=false &&
+		(b1.value!="" && b2.value!="" && b3.value!="" && b1.value==b2.value  && b2.value==b3.value)||
+		(b4.value!="" && b5.value!="" && b6.value!="" && b4.value==b5.value  && b5.value==b6.value)||
+		(b7.value!="" && b8.value!="" && b9.value!="" && b7.value==b8.value  && b8.value==b9.value)||
+		(b1.value!="" && b4.value!="" && b7.value!="" && b1.value==b4.value  && b4.value==b7.value)||
+		(b2.value!="" && b5.value!="" && b8.value!="" && b2.value==b5.value  && b5.value==b8.value)||
+		(b3.value!="" && b6.value!="" && b9.value!="" && b3.value==b6.value  && b6.value==b9.value)||
+		(b1.value!="" && b5.value!="" && b9.value!="" && b1.value==b5.value  && b5.value==b9.value)||
+		(b3.value!="" && b5.value!="" && b7.value!="" && b3.value==b5.value  && b5.value==b7.value)
+		)
+	{
+		gameEnded=true;
+		alert(winner + " won");
+		j=0;
+			}
+	}
+  i++;
+ }
+
+
+  
+  
+
+
+
+
+
+
